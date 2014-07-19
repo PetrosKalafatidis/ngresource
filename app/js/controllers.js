9251edb05c984ca47cb0c94de2bbe6424b71328f
Petran <calafatidis@gmail.com>
2014-07-19 01:43:26 -0400

    resource example

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('myApp.controllers', ['ngResource'])
+  .controller('MyCtrl1', ['$scope','User', function($scope, User) {
+
+    $scope.users = User.query();
+  	$scope.get_user = function( user_id ) {
+      $scope.the_user = User.show({user_id: user_id});
+  	};
 
   }])
+
   .controller('MyCtrl2', ['$scope', function($scope) {
 
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,21 @@
 'use strict';
 
 /* Services */
+angular.module('myApp.services', ['ngResource'])
+    .factory('User', function ($resource, $http) {
+        $http.defaults.headers.common['JsonStub-User-Key']= '1e4a7060-13f1-4827-b6c7-67b533ec5f36';
+        $http.defaults.headers.common['JsonStub-Project-Key']= '82711cd3-b1c8-4a40-8372-578f30a19558';
 
+        return  $resource('http://jsonstub.com/resource/:user_id', {}, {
+        show: { method: 'GET', params: { user_id: '@id' } },
+        update: { method: 'PUT', params: { user_id: '@id' } },
+        delete: { method: 'DELETE', params: { user_id: '@id' } },
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+        })
+    })
+
+   .value('version', '0.1');
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
